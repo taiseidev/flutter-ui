97993c8f1f64aa6acd967127615016340cb0b269
onishi taisei <taisei.developer@gmail.com>
2022-10-15 01:33:45 +0900

    トップ画面を作成

---
+++ lib/loginScreen/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/loginScreen/constants.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Auth Screen',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: const TextTheme(
+          headline4: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          headline6: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: false,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/loginScreen/login-screen-background.jpeg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    "BAKING LESSONS",
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  Text(
+                    "MASTER THE ART OF BAKING",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  const Spacer(),
+                  FittedBox(
+                    child: GestureDetector(
+                      // onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: ((context) => ))),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 26,
+                          vertical: 16,
+                        ),
+                        decoration: const BoxDecoration(
+                          color: kPrimaryColor,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              'START LEARNING',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .button!
+                                  .copyWith(color: Colors.black),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            const Icon(Icons.arrow_forward)
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
