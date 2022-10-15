9aa9c8ff5d253da18bf6ca6376e14ec351169989
onishi taisei <taisei.developer@gmail.com>
2022-10-15 13:52:19 +0900

    タブを作成

---
+++ lib/FlutterDayAndNightMoodAnimation/screens/tabs.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../size_config.dart';
+
+class Tabs extends StatelessWidget {
+  const Tabs({
+    super.key,
+    required this.onTap,
+  });
+
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: SizeConfig.screenWidth! * 0.8,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        color: Colors.white24,
+      ),
+      child: DefaultTabController(
+        length: 2,
+        child: TabBar(
+          // タブのスタイルを指定
+          indicator: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          indicatorColor: Colors.white,
+          labelStyle: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.white,
+          onTap: onTap,
+          tabs: const [
+            Tab(
+              text: 'Morning Login',
+            ),
+            Tab(
+              text: 'Night Login',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
